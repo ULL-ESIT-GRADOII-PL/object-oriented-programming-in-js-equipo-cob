594ab9b6eccde620e68eaecd67300a5ebeec87b5
Carlos Troyano Carmona <carlos.troyano.carmona@gmail.com>
2016-02-23 11:55:26 +0000

    Creadas funciones en jquery para mostrar y ocultar los tags

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
-
+/** cambiando opacidad de las fotos textos  */
 
 function lastChildOpaciti(nodoLlamador, opacity, opacity2) {
     nodoLlamador.childNodes[3].style.opacity = opacity;
-    console.log( nodoLlamador.childNodes);
     nodoLlamador.childNodes[1].style.opacity = opacity2;
 }
+
+/* función que comprueba si un elmento es visible con la propiedad display */
+function isVisible(elemento) {
+    if($(elemento).is(":visible"))
+        return true;
+    else
+        return false;
+}
+
+/*  función para mostrar y ocultar tags además de cambiar el subrayado*/
+function ocultarMostrarTags(focus) {
+    var collection = $("article");
+        $(focus).addClass("subrayado");
+        $(focus).parent().siblings("li").children("a").removeClass("subrayado"); /** acceso mediante familia */
+
+        if(focus.id == "e1") {
+            $(".contendeorImagenesEnlace").show(500);
+            $(".conversorDeTemperatura").hide(500);
+            $(".conversorDeTemperaturaTest").hide(500);
+        }
+        if(focus.id == "e2") {
+            $(".contendeorImagenesEnlace").hide(500);
+            $(".conversorDeTemperatura").show(500);
+            $(".conversorDeTemperaturaTest").hide(500);
+        }
+        if(focus.id == "e3") {
+            $(".contendeorImagenesEnlace").hide(500);
+            $(".conversorDeTemperatura").hide(500);
+            $(".conversorDeTemperaturaTest").show(500);
+        }
+}
+
+/** función principal de js */
 
 function main() {
     var opaco = false;
@@ -17,7 +49,12 @@ function main() {
             lastChildOpaciti(this, '-1', '1');
         } , false);
     }
-    
-}
+     $(document).ready(function() {
+	    $(".menuHEader li a").on( "click", function() {
+	        ocultarMostrarTags(this);
+	    });
+     });
 
+
+}
 
