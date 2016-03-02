9a9d0add335085f12e9b9be2905f682aa747b81f
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2016-03-02 23:00:38 +0000

    deplazado parte del codigo de conversion a sus respectivos tipos de temperatura

--- js/temperature.js
+++ js/temperature.js
@@ -9,13 +9,17 @@ function Medida() {
 //Clase Temperatura con herencia de Medida -constructor
 function Temperatura() {
     Medida.call(this);
-
 }
 
 Temperatura.prototype = Object.create(Medida.prototype);
 Temperatura.prototype.constructor = Temperatura;
 
+//Calculo de la conversion de temperaturas
 Temperatura.prototype.calculate = function(original) {
+  var celsius = new Celsius();
+  var fahrenheit = new Fahrenheit();
+  var kelvin = new Kelvin();
+
   var result = ["","",""];
   var temp = original.value;
   // var regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:[eE][-+]?\d+)?)\s*([cCfFkK])\s*/
@@ -35,32 +39,14 @@ Temperatura.prototype.calculate = function(original) {
     }
     var type = m[2];
     if (m[2] == 'c' || m[2] == 'C') {
-      result[0] = num ;
-      result[0] = result[0].toFixed(2)+" C";
-      result[1] = (num * 9/5)+32;
-      result[1] = result[1].toFixed(2)+" F";
-      result[2] = (num + 273.15) ;
-      result[2] = result[2].toFixed(2)+" K";
-
+      result = celsius.calculate(m,num);
     }
     else {
       if (m[2] == 'f' || m[2] == 'F') {
-        result[0] = (num - 32)*5/9;
-        result[0]= result[0].toFixed(2)+" C";
-
-        result[1] = num;
-        result[1] = result[1].toFixed(2)+" F";
-
-        result[2] = ((num - 32)*5/9)+273.15;
-        result[2] = result[2].toFixed(2)+" K";
+        result = fahrenheit.calculate(m,num);
       }
       else {
-        result[0] = (num - 273.15);
-        result[0]= result[0].toFixed(2)+" C";
-        result[1] = ((num - 273.15)/(5/9))+32;
-        result[1] = result[1].toFixed(2)+" F";
-        result[2] = num ;
-        result[2] = result[2].toFixed(2)+" K";
+        result = kelvin.calculate(m,num);
       }
     }
     return result;
@@ -76,6 +62,21 @@ function Celsius() {
 Celsius.prototype = Object.create(Temperatura.prototype)
 Celsius.prototype.constructor = Celsius;
 
+//Sobreescribiendo calculate de Temperatura
+Celsius.prototype.calculate = function(m,num) {
+  this.valor = m[1];
+  this.tipo = m[2];
+  var result = ["","",""];
+
+  result[0] = num ;
+  result[0] = result[0].toFixed(2)+" C";
+  result[1] = (num * 9/5)+32;
+  result[1] = result[1].toFixed(2)+" F";
+  result[2] = (num + 273.15) ;
+  result[2] = result[2].toFixed(2)+" K";
+  return result;
+}
+
 //Clase Fahrenheit con herencia de Temperatura -constructor
 function Fahrenheit() {
   Temperatura.call(this);
@@ -84,6 +85,22 @@ function Fahrenheit() {
 
 Fahrenheit.prototype = Object.create(Temperatura.prototype)
 Fahrenheit.prototype.constructor = Fahrenheit;
+
+//Sobreescribiendo calculate de Temperatura
+Fahrenheit.prototype.calculate = function(m,num) {
+  this.valor = m[1];
+  this.tipo = m[2];
+  var result = ["","",""];
+
+  result[0] = (num - 32)*5/9;
+  result[0]= result[0].toFixed(2)+" C";
+  result[1] = num;
+  result[1] = result[1].toFixed(2)+" F";
+  result[2] = ((num - 32)*5/9)+273.15;
+  result[2] = result[2].toFixed(2)+" K";
+
+  return result;
+}
 
 //Clase Kelvin con herencia de Temperatura -constructor
 function Kelvin() {
@@ -94,4 +111,21 @@ function Kelvin() {
 Kelvin.prototype = Object.create(Temperatura.prototype)
 Kelvin.prototype.constructor = Kelvin;
 
+//Sobreescribiendo calculate de Temperatura
+Kelvin.prototype.calculate = function(m,num) {
+  this.valor = m[1];
+  this.tipo = m[2];
+  var result = ["","",""];
+
+  result[0] = (num - 273.15);
+  result[0]= result[0].toFixed(2)+" C";
+  result[1] = ((num - 273.15)/(5/9))+32;
+  result[1] = result[1].toFixed(2)+" F";
+  result[2] = num ;
+  result[2] = result[2].toFixed(2)+" K";
+
+  return result;
+}
+
+//Variable a usar en el main para comerzar la conversion
 var temperatura = new Temperatura();
